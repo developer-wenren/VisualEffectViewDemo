06d138eb038e25c6840f061a4844cccab1010d11
wrcj <wrcj12138@163.com>
2015-05-25 14:12:07 +0800

    Initial Commit

---
+++ VisualEffectView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VisualEffectView
+//
+//  Created by zjsruxxxy3 on 15/5/25.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VisualEffectView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VisualEffectView
+//
+//  Created by zjsruxxxy3 on 15/5/25.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
